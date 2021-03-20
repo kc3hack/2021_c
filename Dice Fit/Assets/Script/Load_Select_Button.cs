ee978b37e144d8e4f3c4e8dbbe5e6b83f87050f8
CrowleyXavier <janjanjanjan5738@ezweb.ne.jp>
2021-03-21 04:26:23 +0900

    StartScene
    スタート画面の実装

---
+++ Dice Fit/Assets/Script/Load_Select_Button.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Load_Select_Button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Select_Menu");
+    }
+
+}
